a0f58a76314b993876064b42f6fda1b90e33a707
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2019-03-13 22:43:17 +0330

    Add forms

--- js/main.js
+++ js/main.js
@@ -158,94 +158,94 @@ $(document).ready(function($) {
     /*----------------------------------------------------*/
     /*  Contact Form
     /*----------------------------------------------------*/
-
-    $("#contact").submit(function(e) {
-        e.preventDefault();
-        var name = $("#name").val();
-        var email = $("#email").val();
-        var subject = $("#subject").val();
-        var message = $("#message").val();
-        var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
-
-        function isValidEmail(emailAddress) {
-            var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
-            return pattern.test(emailAddress);
-        };
-
-        if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
-            $.ajax({
-                type: "POST",
-                url: "sendmail.php",
-                data: dataString,
-                success: function() {
-                    $('.success').fadeIn(1000);
-                    $('.error').fadeOut(500);
-                }
-            });
-        } else {
-            $('.error').fadeIn(1000);
-            $('.success').fadeOut(500);
-        }
-
-        return false;
-    });
+    //
+    // $("#contact").submit(function(e) {
+    //     e.preventDefault();
+    //     var name = $("#name").val();
+    //     var email = $("#email").val();
+    //     var subject = $("#subject").val();
+    //     var message = $("#message").val();
+    //     var dataString = 'name=' + name + '&email=' + email + '&subject=' + subject + '&message=' + message;
+    //
+    //     function isValidEmail(emailAddress) {
+    //         var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
+    //         return pattern.test(emailAddress);
+    //     };
+    //
+    //     if (isValidEmail(email) && (message.length > 1) && (name.length > 1)) {
+    //         $.ajax({
+    //             type: "GET",
+    //             url: "https://subs.isapanah.com/",
+    //             data: dataString,
+    //             success: function() {
+    //                 $('.success').fadeIn(1000);
+    //                 $('.error').fadeOut(500);
+    //             }
+    //         });
+    //     } else {
+    //         $('.error').fadeIn(1000);
+    //         $('.success').fadeOut(500);
+    //     }
+    //
+    //     return false;
+    // });
     /*----------------------------------------------------*/
     /*  Subscribe
     /*----------------------------------------------------*/
-    ajaxMailChimpForm($("#newsletter"), $("#subscribe-result"));
-    // Turn the given MailChimp form into an ajax version of it.
-    function ajaxMailChimpForm($form, $resultElement) {
-        $form.submit(function(e) {
-            e.preventDefault();
-            if (!isValidEmail($form)) {
-                var error = "A valid email address must be provided.";
-                $resultElement.html(error);
-                $resultElement.css("color", "red");
-            } else {
-                $resultElement.css("color", "black");
-                $resultElement.html("Subscribing...");
-                submitSubscribeForm($form, $resultElement);
-            }
-        });
-    }
+    // ajaxMailChimpForm($("#newsletter"), $("#subscribe-result"));
+    // // Turn the given MailChimp form into an ajax version of it.
+    // function ajaxMailChimpForm($form, $resultElement) {
+    //     $form.submit(function(e) {
+    //         e.preventDefault();
+    //         if (!isValidEmail($form)) {
+    //             var error = "A valid email address must be provided.";
+    //             $resultElement.html(error);
+    //             $resultElement.css("color", "red");
+    //         } else {
+    //             $resultElement.css("color", "black");
+    //             $resultElement.html("Subscribing...");
+    //             submitSubscribeForm($form, $resultElement);
+    //         }
+    //     });
+    // }
     // Validate the email address in the form
-    function isValidEmail($form) {
-        // If email is empty, show error message.
-        var email = $form.find("input[type='email']").val();
-        if (!email || !email.length) {
-            return false;
-        } else if (email.indexOf("@") == -1) {
-            return false;
-        }
-        return true;
-    }
+    // function isValidEmail($form) {
+    //     // If email is empty, show error message.
+    //     var email = $form.find("input[type='email']").val();
+    //     if (!email || !email.length) {
+    //         return false;
+    //     } else if (email.indexOf("@") == -1) {
+    //         return false;
+    //     }
+    //     return true;
+    // }
 
-    function submitSubscribeForm($form, $resultElement) {
-        $.ajax({
-            type: "GET",
-            url: $form.attr("action"),
-            data: $form.serialize(),
-            cache: false,
-            dataType: "jsonp",
-            jsonp: "c", // trigger MailChimp to return a JSONP response
-            contentType: "application/json; charset=utf-8",
-            error: function(error) {
-
-            },
-            success: function(data) {
-                if (data.result != "success") {
-                    var message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
-                    $resultElement.css("color", "red");
-                    if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
-                        message = "You're already subscribed. Thank you.";
-                        $resultElement.css("color", "black");
-                    }
-                    $resultElement.html(message);
-                } else {
-                    $resultElement.css("color", "black");
-                    $resultElement.html("Thank you!<br>You must confirm the subscription in your inbox.");
-                }
-            }
-        });
-    }
+    // function submitSubscribeForm($form, $resultElement) {
+    //     $.ajax({
+    //         type: "GET",
+    //         url: $form.attr("action"),
+    //         data: $form.serialize(),
+    //         cache: false,
+    //         dataType: "jsonp",
+    //         jsonp: "c", // trigger MailChimp to return a JSONP response
+    //         contentType: "application/json; charset=utf-8",
+    //         error: function(error) {
+    //
+    //         },
+    //         success: function(data) {
+    //             if (data.result != "success") {
+    //                 var message = data.msg || "Sorry. Unable to subscribe. Please try again later.";
+    //                 $resultElement.css("color", "red");
+    //                 if (data.msg && data.msg.indexOf("already subscribed") >= 0) {
+    //                     message = "You're already subscribed. Thank you.";
+    //                     $resultElement.css("color", "black");
+    //                 }
+    //                 $resultElement.html(message);
+    //             } else {
+    //                 $resultElement.css("color", "black");
+    //                 $resultElement.html("Thank you!<br>You must confirm the subscription in your inbox.");
+    //             }
+    //         }
+    //     });
+    // }
 });
---
+++ js/utils.js
@@ -0,0 +1,93 @@
+
+
+var $ = jQuery.noConflict();
+
+//Subscribe
+function subscribeUser()
+{
+    var selection = document.getElementById("newsletter").elements[0].value;
+    var email = document.getElementById("newsletter").elements[1].value;
+    var name = document.getElementById("newsletter").elements[2].value;
+
+    document.getElementById("newsletter-form-input").style.display = "none";
+    document.getElementById("newsletter-form-error").style.display = "none";
+
+    if(
+        validateEmail(email) &&
+        name && name.length && name.trim().length > 0
+    ){
+        sendSubscribeRequest({
+            description: selection,
+            email: email,
+            name: name,
+            source: "solid.isapanah.com",
+            campaign: "site"
+        })
+    }else{
+        document.getElementById("newsletter-form-input").style.display = "flex";
+    }
+}
+
+function sendSubscribeRequest(prams)
+{
+    $.post( "https://subs.isapanah.com/newsletter/register", prams)
+        .done(function( data ) {
+            document.getElementById("newsletter").style.display = "none";
+            $("#newsletter-container").after(
+                '<br/><div class="alert alert-success"><strong>با تشکر، فصل انتخابی به ایمیل شما ارسال شد. لطفا فولدر smap را هم چک کنید.</strong></div>'
+            )
+        }).fail(function() {
+            document.getElementById("newsletter-form-error").style.display = "flex";
+        })
+}
+
+//Contact
+function contact()
+{
+    var name = document.getElementById("contact").elements[0].value;
+    var email = document.getElementById("contact").elements[1].value;
+    var subject = document.getElementById("contact").elements[2].value;
+    var message = document.getElementById("contact").elements[3].value;
+
+
+    document.getElementById("contact-form-error").style.display = "none";
+    document.getElementById("contact-form-input").style.display = "none";
+
+    if(
+        validateEmail(email) &&
+        message && message.length && message.trim().length > 0
+    ){
+        sendNewMessage({
+            name: name,
+            email: email,
+            subject: subject,
+            message: message,
+            source: "solid.isapanah.com"
+        })
+    }else{
+        document.getElementById("contact-form-input").style.display = "flex";
+    }
+}
+
+
+function sendNewMessage(prams)
+{
+    $.post( "https://subs.isapanah.com/contact/new", prams)
+        .done(function( data ) {
+            document.getElementById("contact").style.display = "none";
+            $("#contact-container").after(
+                '<br/><div class="alert alert-success"><strong>پیام شما با موفقیت دریافت شد. درصورت لزوم با شما تماس خواهیم گرفت.</strong></div>'
+            )
+        }).fail(function() {
+            document.getElementById("contact-form-error").style.display = "flex";
+        })
+}
+
+
+
+
+// Validate the email address in the form
+function validateEmail(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
