4b0becb9154e1595ad0978f698d96f2c0d7ffc20
Sadra Isapanah Amlashi <amlashi.sadra@gmail.com>
2019-03-16 00:21:19 +0330

    Modifu font-awesome

--- js/jquery.prettyPhoto.js
+++ js/jquery.prettyPhoto.js
@@ -47,21 +47,21 @@
 								<div class="pp_content"> \
 									<div class="pp_loaderIcon"></div> \
 									<div class="pp_fade"> \
-										<a href="#" class="pp_expand" title="Expand the image">Expand</a> \
+										<a href="#" class="pp_expand" title="Expand the image"><i class="fas fa-expand-arrows-alt"></i></a> \
 										<div class="pp_hoverContainer"> \
 											<a class="pp_next" href="#">next</a> \
-											<a class="pp_previous" href="#">previous</a> \
+											<a class="pp_previous" href="#"><i class="fas fa-arrow-circle-left"></i></a> \
 										</div> \
 										<div id="pp_full_res"></div> \
 										<div class="pp_details"> \
 											<div class="pp_nav"> \
-												<a href="#" class="pp_arrow_previous">Previous</a> \
+												<a href="#" class="pp_arrow_previous"><i class="fas fa-arrow-circle-left"></i></a> \
 												<p class="currentTextHolder">0/0</p> \
-												<a href="#" class="pp_arrow_next">Next</a> \
+												<a href="#" class="pp_arrow_next"><i class="fas fa-arrow-circle-right"></i></a> \
 											</div> \
 											<p class="pp_description"></p> \
 											<div class="pp_social">{pp_social}</div> \
-											<a class="pp_close" href="#">Close</a> \
+											<a class="pp_close" href="#"><i class="far fa-times-circle"></i></a> \
 										</div> \
 									</div> \
 								</div> \
